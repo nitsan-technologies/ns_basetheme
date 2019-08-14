e4c91b5016b141c4a5ea25393ab0999a031bc33e
Sanjay Chauhan <sanjay@nitsan.in>
2019-08-14 13:03:04 +0530

    [FEATURE] Added dynamic settings and view for CType:record

--- Configuration/TypoScript/Components/ns_record.ts
+++ Configuration/TypoScript/Components/ns_record.ts
@@ -1,34 +1,54 @@
-lib {
-    // Get Record content
-    getRecord = FLUIDTEMPLATE
-    getRecord {
+// Initiate TS to pass Data to Fluid
+NsRecordList = FLUIDTEMPLATE
+NsRecordList {
 
-        // Set filename
-        file = EXT:ns_basetheme/Resources/Private/Components/NsRecord.html
+    // Set filename
+    file = EXT:ns_basetheme/Resources/Private/Components/NsRecord.html
+    
+    // Data Processing
+    dataProcessing.10 = TYPO3\CMS\Frontend\DataProcessing\DatabaseQueryProcessor
+    dataProcessing.10 {
         
-        // Data Processing
-        dataProcessing.10 = TYPO3\CMS\Frontend\DataProcessing\DatabaseQueryProcessor
-        dataProcessing.10 {
-            
-            // Define your DB table
-            table = tx_nsbasetheme_domain_model_record
-            pidInList = {field:pid}
-            pidInList.insertData = 1       
-            uidInList = {field:uid}
-            uidInList.insertData = 1
+        // Define your DB table
+        table = tx_nsbasetheme_domain_model_record
+        pidInList = {field:pid}
+        pidInList.insertData = 1       
+        uidInList = {field:uid}
+        uidInList.insertData = 1
 
-            // Apply your special where claus
-            // where = field=value
+        // Apply your special where claus
+        // where = field=value
 
-            // Pass as Object to Fluid
-            as = NsRecord
+        // Pass as Object to Fluid
+        as = NsRecordList
 
-            dataProcessing {
-                10 = TYPO3\CMS\Frontend\DataProcessing\FilesProcessor
-                10 {
-                        references.fieldName = image
-                }
+        // Pass Flexform data eg., Detail page insertData
+        variables {
+            detailPageId = TEXT
+            detailPageId.value = {field:detailPageId}
+            detailPageId.insertData = 1
+        }
+
+        // Process the files and images
+        dataProcessing {
+            10 = TYPO3\CMS\Frontend\DataProcessing\FilesProcessor
+            10 {
+                    references.fieldName = image
             }
         }
     }
+}
+
+// Let's just copy RecordLis and do the needful adaption
+NsRecordDetail < NsRecordList
+NsRecordDetail {
+    dataProcessing.10 {
+        
+        // Set GET Params
+        uidInList = {GP:uid}
+        uidInList.insertData = 1              
+
+        // Pass with different Object
+        as = NsRecordDetail
+    }   
 }
--- ext_tables.php
+++ ext_tables.php
@@ -9,5 +9,6 @@ defined('TYPO3_MODE') or die();
     'Default TYPO3 Theme & Templates'
 );
 
+// Add CType: Record
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_nsbasetheme_domain_model_record', 'EXT:ns_basetheme/Resources/Private/Language/locallang_csh_tx_nsbasetheme_domain_model_record.xlf');
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_nsbasetheme_domain_model_record');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_nsbasetheme_domain_model_record');
