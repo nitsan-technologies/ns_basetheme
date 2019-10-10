3461b1861c7a5a9ed1c49b5bf62616e6686d0ecf
Keval Pandya <keval@nitsan.in>
2019-10-10 15:30:03 +0000

    Update ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,7 @@
 <?php
 // TYPO3 Security Check
 defined('TYPO3_MODE') or die();
-
+$_EXTKEY = 'ns_basetheme';
 // Add default include static TypoScript (for root page)
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     $_EXTKEY,
