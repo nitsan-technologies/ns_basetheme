e57688e7e0ddfb8b0ecdcb004eab2c4b150c5b26
Nilesh Malankiya <mnilesh.nitsan@gmail.com>
2020-04-22 11:49:14 +0530

    [TODO] Move module after the Web Module

--- ext_tables.php
+++ ext_tables.php
@@ -8,27 +8,26 @@ $_EXTKEY = 'ns_basetheme';
 if (TYPO3_MODE === 'BE') {
     $isVersion9Up = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 9000000;
 
-    
     if (version_compare(TYPO3_branch, '8.0', '>=')) {
 
-    	// Add module 'nitsan' after 'Web'
-	    if (!isset($GLOBALS['TBE_MODULES']['nitsan'])) {
-	        $temp_TBE_MODULES = [];
-	        foreach ($GLOBALS['TBE_MODULES'] as $key => $val) {
-	            if ($key == 'web') {
-	                $temp_TBE_MODULES[$key] = $val;
-	                $temp_TBE_MODULES['nitsan'] = '';
-	            } else {
-	                $temp_TBE_MODULES[$key] = $val;
-	            }
-	        }
-	        $GLOBALS['TBE_MODULES'] = $temp_TBE_MODULES;
-	        $GLOBALS['TBE_MODULES']['_configuration']['nitsan'] = [
-            	'iconIdentifier' => 'module-nsbasetheme',
-            	'labels' => 'LLL:EXT:ns_basetheme/Resources/Private/Language/BackendModule.xlf',
-            	'name' => 'nitsan'
-        	];
-	    }
+        // Add module 'nitsan' after 'Web'
+        if (!isset($GLOBALS['TBE_MODULES']['nitsan'])) {
+            $temp_TBE_MODULES = [];
+            foreach ($GLOBALS['TBE_MODULES'] as $key => $val) {
+                if ($key == 'web') {
+                    $temp_TBE_MODULES[$key] = $val;
+                    $temp_TBE_MODULES['nitsan'] = '';
+                } else {
+                    $temp_TBE_MODULES[$key] = $val;
+                }
+            }
+            $GLOBALS['TBE_MODULES'] = $temp_TBE_MODULES;
+            $GLOBALS['TBE_MODULES']['_configuration']['nitsan'] = [
+                'iconIdentifier' => 'module-nsbasetheme',
+                'labels' => 'LLL:EXT:ns_basetheme/Resources/Private/Language/BackendModule.xlf',
+                'name' => 'nitsan'
+            ];
+        }
 
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
             'NITSAN.NsBasetheme',
