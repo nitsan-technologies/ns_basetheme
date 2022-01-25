32f7a054186ea0673596db3a52d7f635eab97156
Sanjay Chauhan <sanjay@nitsan.in>
2022-01-25 18:25:48 +0530

    [TODO] Migrated everything from ext_localconf.php to Classes/NsBasethemeUtility.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,13 +3,13 @@
 // Provide detailed information and depenencies of EXT:ns_basetheme
 $EM_CONF['ns_basetheme'] = [
     'title' => '[NITSAN] TYPO3 Base Template',
-    'description' => 'The architecture of base and child theme concept. Explore https://t3terminal.com/ and https://www.nitsan.in/',
+    'description' => 'The architecture of parent/child TYPO3 theme concept. Explore https://t3terminal.com and https://nitsantech.com',
     'category' => 'templates',
     'author' => 'Team NITSAN',
     'author_email' => 'sanjay@nitsan.in',
     'author_company' => 'NITSAN Technologies Pvt Ltd',
     'state' => 'stable',
-    'version' => '11.0.1',
+    'version' => '11.1.0',
     'constraints' => [
         'depends' => [
             'typo3' => '8.0.0-11.5.99'
--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,8 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
+
+// Setup for before and after extension Installation
 $_EXTKEY = 'ns_basetheme';
 if (TYPO3_MODE === 'BE' && version_compare(TYPO3_branch, '8.0', '>') && version_compare(TYPO3_branch, '9.0', '<')) {
     $class = 'TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher';
@@ -24,193 +26,45 @@ if (TYPO3_MODE === 'BE' && version_compare(TYPO3_branch, '8.0', '>') && version_
     );
 }
 
-// Let's configuration of this extension from "Extension Manager"
+// Let's get configuration of this extension from "Extension Manager"
 // $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = unserialize($_EXTCONF);
 
-// Define your each componenet's flexform files
-$allComponents = [];
-if (version_compare(TYPO3_branch, '9.0', '>')) {
-    $siteRoot = TYPO3\CMS\Core\Core\Environment::getPublicPath() . '/typo3conf/';
-    $arrAllComponents['ns_basetheme'] = scandir(\TYPO3\CMS\Core\Core\Environment::getPublicPath() . '/typo3conf/ext/ns_basetheme/Configuration/FlexForms');
-} else {
-    $siteRoot = PATH_typo3conf;
-    $arrAllComponents['ns_basetheme'] = scandir(PATH_typo3conf . 'ext/ns_basetheme/Configuration/FlexForms');
-}
-$arrAllExtensions = [];
-$activePackages = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Package\PackageManager::class)->getActivePackages();
-foreach ($activePackages as $package) {
-    $extensionPrefixKey = substr($package->getPackageKey(), 0, 9);
-    if ($extensionPrefixKey == 'ns_theme_') {
-        $arrAllExtensions[] = $package->getPackageKey();
-    }
-}
+// Initiate NsBaseThemeUtility
+$objNsBasetheme = TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\NITSAN\NsBasetheme\NsBasethemeUtility::class);
 
-if ($_COOKIE['NsLicense'] != '') {
-    $disableExtensions = explode(',', $_COOKIE['NsLicense']);
-    foreach ($disableExtensions as $ext) {
-        $key = array_search($ext, $arrAllExtensions);
-        unset($arrAllExtensions[$key]);
-    }
-}
-foreach ($arrAllExtensions as $key => $extKey) {
-    // Get only extension which are child theme eg., EXT:ns_theme_cleanblog
-    $extensionPrefixKey = substr($extKey, 0, 9);
-    if ($extensionPrefixKey == 'ns_theme_') {
-        if (version_compare(TYPO3_branch, '9.0', '>')) {
-            $arrAllComponents[$extKey] = scandir(\TYPO3\CMS\Core\Core\Environment::getPublicPath() . "/typo3conf/ext/$extKey/Configuration/FlexForms");
-        } else {
-            $arrAllComponents[$extKey] = scandir(PATH_typo3conf . "ext/$extKey/Configuration/FlexForms");
-        }
-    }
-}
-if (array_key_exists('ns_theme_extend', $arrAllComponents)) {
-    $themeExtend = $arrAllComponents['ns_theme_extend'];
-    unset($arrAllComponents['ns_theme_extend']);
-    $arrAllComponents['ns_theme_extend'] = $themeExtend;
-}
-// Preparing final array with ALL components from ALL themes
-foreach ($arrAllComponents as $extKey => $extValue) {
-    foreach ($extValue as $key => $value) {
-        if ($value != '.' && $value != '..' && strpos($value, '.xml') !== false) {
-            $theComponentName = str_replace('.xml', '', $value);
-            if (!empty($theComponentName)) {
-                $allComponents[$extKey][] = $theComponentName;
-            }
-        }
-    }
-}
+// Initiate with blank list of themes + components
+$allComponents = $arrAllExtensions = [];
+
+// Get list of all the themes which starts from EXT.ns_theme_*
+$arrAllExtensions = $objNsBasetheme->getInstalledChildTheme();
+
+// Get list of all the components from EXT.ns_theme_*
+$allComponents = $objNsBasetheme->getChildThemeComponents($arrAllExtensions);
+
+// Settled constatant to access from "Everywhere"
+define('ALL_COMPONENTS', $allComponents);
+
+// Are you in Backend?
 if (TYPO3_MODE === 'BE') {
-    // Let's check if our child themes are available
-    if (count($arrAllExtensions) > 0) {
-        foreach ($arrAllExtensions as $key => $extKey) {
-            // Get only extension which are child theme eg., EXT:ns_theme_cleanblog
-            $extensionPrefixKey = substr($extKey, 0, 9);
-            if ($extensionPrefixKey == 'ns_theme_') {
-                // Render Custom CSS and Javascript
-                $renderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
-                $css = $siteRoot . 'ext/' . $extKey . '/Resources/Public/Backend/Css/Backend.css';
-                $js = $siteRoot . 'ext/' . $extKey . '/Resources/Public/Backend/JavaScript/Backend.js';
-                if (file_exists($css)) {
-                    $renderer->addCssFile('EXT:' . $extKey . '/Resources/Public/Backend/Css/Backend.css', 'stylesheet', 'all');
-                }
-                if (file_exists($js)) {
-                    $renderer->addJsFile('EXT:' . $extKey . '/Resources/Public/Backend/JavaScript/Backend.js', 'text/javascript', false);
-                }
-                unset($css);
-                unset($js);
-            }
-        }
-    }
+
+    // Let's make beautiful backend preview by adding custom CSS/JS
+    $objNsBasetheme->setupBackendPreviewCssJs($arrAllExtensions);
+
+    // Let's prepare CType components to add at PageTS Config
+    $pageTSConfig = $objNsBasetheme->prepareWizardPageTSConfig($allComponents);
+
+    // Adding final CType and extra tab call "Custom Components"
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($pageTSConfig);
+
+    // Let's add default PageTS for "Form"
+    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:ns_basetheme/Configuration/RTE/Default.yaml';
 }
 
 // Let's add default PageTSConfig for Backend layout, TCE form, Components etc.,
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTSconfig/setup.typoscript">');
 
-
-// Settled constatant to access from "Everywhere"
-define('ALL_COMPONENTS', $allComponents);
-
-// Include new content elements to modWizards
-if (TYPO3_MODE === 'BE') {
-    call_user_func(
-        function ($_EXTKEY) {
-            // Get Components from ext_localconf.php
-            $allComponents = constant('ALL_COMPONENTS');
-
-            // Let's prepare CType components to add at PageTS Config
-            $collectComponent = $listComponent = $tsComponents = '';
-            foreach ($allComponents as $extKey => $extValue) {
-                foreach ($extValue as $key => $theComponent) {
-                    $collectComponent .= "
-                    $theComponent {
-                      iconIdentifier = $theComponent
-                      title = LLL:EXT:$extKey/Resources/Private/Language/locallang_db.xlf:wizard.$theComponent
-                      description = LLL:EXT:$extKey/Resources/Private/Language/locallang_db.xlf:wizard.$theComponent.desc
-                      tt_content_defValues {
-                          CType = $theComponent
-                      }
-                    }
-                ";
-                    $listComponent .= $theComponent . ',';
-                    $tsComponents .= '
-                    ' . $theComponent . ' < .ns_default
-                    ' . $theComponent . '.templateName = ' . ucfirst($theComponent) . '
-                ';
-                }
-            }
-
-            // Adding final CType and extra tab call "Custom Components"
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig("
-                # Add new custom wizard for our Components
-                mod.wizards.newContentElement.wizardItems.extra {
-
-                   # Set caption
-                   header = Custom Elements
-                   icon =
-
-                   # Register each Elements
-                   elements {
-                        $collectComponent
-                   }
-                   show := addToList($listComponent)
-                }
-            ");
-        },
-        $_EXTKEY
-    );
-    // Let's add default PageTS for "Form"
-    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:ns_basetheme/Configuration/RTE/Default.yaml';
-}
-
 // Let's prepare CType components to add at TypoScript Config
-$tsComponents = '';
-foreach ($allComponents as $extKey => $extValue) {
-    foreach ($extValue as $key => $theComponent) {
-        $arrTemplateName = explode('_', $theComponent);
-        $templateName = ucfirst($arrTemplateName[0]) . '' . ucfirst($arrTemplateName[1]);
-        if (!empty($templateName)) {
-            $tsComponents .= "
-                $theComponent = FLUIDTEMPLATE
-                $theComponent {
-                    templateRootPaths {
-                        0 = EXT:fluid_styled_content/Resources/Private/Templates/
-                        10 = EXT:$extKey/Resources/Private/Components/
-                    }
-                    partialRootPaths {
-                        0 = EXT:fluid_styled_content/Resources/Private/Partials/
-                        10 = EXT:$extKey/Resources/Private/Partials/
-                    }
-                    variables {
-                    }
-                    templateName = $templateName
-                    dataProcessing {
-                        10 = TYPO3\CMS\Frontend\DataProcessing\FilesProcessor
-                        10 {
-                            references.fieldName = image
-                            as = image
-                        }
-                        20 = TYPO3\CMS\Frontend\DataProcessing\FilesProcessor
-                        20 {
-                            references.fieldName = media
-                            as = media
-                        }
-                        30 = TYPO3\CMS\Frontend\DataProcessing\FilesProcessor
-                        30 {
-                            references.fieldName = file2
-                            as = file2
-                        }
-                        40 = TYPO3\CMS\Frontend\DataProcessing\FilesProcessor
-                        40 {
-                            references.fieldName = file3
-                            as = file3
-                        }
-                        50 = NITSAN\\NsBasetheme\\DataProcessing\\DefaultProcessor
-                    }
-                }
-            ";
-        }
-    }
-}
+$tsComponents = $objNsBasetheme->setupComponentWiseTypoScript($allComponents);
 
 // Add TypoScript for tt_content as setup.typoscript
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript($_EXTKEY, 'setup', "
@@ -229,17 +83,22 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php'][
 $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
     \TYPO3\CMS\Core\Imaging\IconRegistry::class
 );
-foreach ($allComponents as $extKey => $extValue) {
-    foreach ($extValue as $key => $theComponent) {
-        $iconRegistry->registerIcon(
-            $theComponent,
-            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-            ['source' => (file_exists($siteRoot . 'ext/' . $extKey . '/Resources/Public/Icons/' . $theComponent . '.png')) ? 'EXT:' . $extKey . '/Resources/Public/Icons/' . $theComponent . '.png' : 'EXT:ns_basetheme/Resources/Public/Icons/default_icon.png']
-        );
+if (count($allComponents) > 0) {
+    foreach ($allComponents as $extKey => $extValue) {
+        
+        if (count($extValue) > 0) {
+            foreach ($extValue as $key => $theComponent) {
+                $iconRegistry->registerIcon(
+                    $theComponent,
+                    \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+                    ['source' => (file_exists($siteRoot . 'ext/' . $extKey . '/Resources/Public/Icons/' . $theComponent . '.png')) ? 'EXT:' . $extKey . '/Resources/Public/Icons/' . $theComponent . '.png' : 'EXT:ns_basetheme/Resources/Public/Icons/default_icon.png']
+                );
+            }
+        }
     }
 }
 
-//Module Icon
+// Let's register module's icon
 $iconRegistry->registerIcon(
     'module-nsbasetheme',
     \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
