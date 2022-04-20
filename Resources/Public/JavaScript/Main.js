7fda1d0e758d3e18f7b4cec74981eb0f39da4c66
Sanjay Chauhan <sanjay@nitsan.in>
2022-04-20 16:59:38 +0000

    [TASK] PATCH: by Sanjay for Backend Preview Image

--- Resources/Public/JavaScript/Main.js
+++ Resources/Public/JavaScript/Main.js
@@ -3,28 +3,12 @@ define([
     'TYPO3/CMS/Backend/Modal',
     'TYPO3/CMS/NsBasetheme/Main',
 ], function ($, Modal) {
-    $('.license-activation .license-activation-latest').on('click', function(e){
-        e.preventDefault();
-        $(this).addClass('active');
-        $('#activation-modal').modal('show');
+    
+    // PATCH: Let's add preview image feature on click change
+    $('.themePreviewSelect').on('change', function() {
+        $('.themePreviewImg_'+$(this).attr('data-id')).attr("src", $( this ).find( "option:selected" ).data( "img-src" ));
     });
-    $('#activation-modal .activation-modal-update').on('click', function(e){
-        var url = $('.license-activation .license-activation-latest.active').attr('href');
-        $('.license-activation .license-activation-latest.active').removeClass('active');
-        window.location = url;
-    });
-    $('.custom-reset').on('click', function(){
-        var that = $(this);
-        that.find('i').addClass('fa-spin');
-        var id = that.attr('data-id');
-        var defaultValue = $("#" + id).attr('data-value');
-        $("#" + id).val(defaultValue);
-        $("#" + id).addClass('form__field');
-        setTimeout(function(){
-            $("#" + id).removeClass('form__field');
-            that.find('i').removeClass('fa-spin');
-        }, 2000);
-    });
+    
     $('.field-info-trigger').on('click', function(){
         $(this).parents('.form-group').find('.field-info-text').slideToggle();
     });
