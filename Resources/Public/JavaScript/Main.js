5bfb6f2dceef171b21e640b2dd19f4c77976c83e
Nilesh Malankiya <mnilesh.nitsan@gmail.com>
2022-06-13 12:43:00 +0530

    :bug: Reset button issue fixed for the backend

--- Resources/Public/JavaScript/Main.js
+++ Resources/Public/JavaScript/Main.js
@@ -26,6 +26,20 @@ define([
         $(this).parents('.form-group').find('.field-info-text').slideToggle();
     });
 
+    $('.custom-reset').on('click', function(){
+        var that = $(this);
+        that.find('i').addClass('fa-spin');
+        var id = that.attr('data-id');
+        var defaultValue = $("#" + id).attr('data-value');
+        $("#" + id).val(defaultValue);
+        $("#" + id).addClass('form__field');
+        setTimeout(function(){
+            $("#" + id).removeClass('form__field');
+            that.find('i').removeClass('fa-spin');
+        }, 2000);
+    });
+
+
     $('#TypoScriptTemplateModuleController').on('submit',function(e){
         require(['TYPO3/CMS/Backend/Notification'], function(Notification) {
             Notification.success('Well done', 'Your configuration is updated successfully.');
