eecbb21c16b96f870e811afab046712076f7fb3b
Sanjay Chauhan <sanjay@nitsan.in>
2022-06-16 05:52:40 +0000

    [TASK] Verify license only for admin logins; Ignore editor users

--- Classes/Hooks/BackendUserLogin.php
+++ Classes/Hooks/BackendUserLogin.php
@@ -14,18 +14,20 @@ class BackendUserLogin
 {
     public function dispatch(array $backendUser)
     {
-        // Let's check license system
-        $isLicenseActivate = GeneralUtility::makeInstance(PackageManager::class)->isPackageActive('ns_license');
-        $this->objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+        if($backendUser['user']['admin'] == 1) {
+            // Let's check license system
+            $isLicenseActivate = GeneralUtility::makeInstance(PackageManager::class)->isPackageActive('ns_license');
+            $this->objectManager = GeneralUtility::makeInstance(ObjectManager::class);
 
-        if ($isLicenseActivate) {
-            $this->nsLicenseModule = $this->objectManager->get(\NITSAN\NsLicense\Controller\NsLicenseModuleController::class);
-            $activePackages = GeneralUtility::makeInstance(PackageManager::class)->getAvailablePackages();
-            foreach ($activePackages as $key => $value) {
-                $exp_key = explode('_theme', $key);
-                if ($exp_key[0] == 'ns') {
-                    if ($key != 'ns_basetheme' && $key != 'ns_license') {
-                        $this->nsLicenseModule->connectToServer($key, 1);
+            if ($isLicenseActivate) {
+                $this->nsLicenseModule = $this->objectManager->get(\NITSAN\NsLicense\Controller\NsLicenseModuleController::class);
+                $activePackages = GeneralUtility::makeInstance(PackageManager::class)->getAvailablePackages();
+                foreach ($activePackages as $key => $value) {
+                    $exp_key = explode('_theme', $key);
+                    if ($exp_key[0] == 'ns') {
+                        if ($key != 'ns_basetheme' && $key != 'ns_license') {
+                            $this->nsLicenseModule->connectToServer($key, 0);
+                        }
                     }
                 }
             }
