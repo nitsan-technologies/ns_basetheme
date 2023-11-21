4bd3fc082cdc95f6bbb85e231abbddbfddeaa0e5
Parth Parmar <parth@nitsantech.com>
2023-11-21 14:51:53 +0000

    [TASK] Update background login image

--- ext_tables.php
+++ ext_tables.php
@@ -3,5 +3,5 @@ defined('TYPO3') or die();
 
 // Add Custom TYPO3 Backend Login Screen
 if (empty($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginBackgroundImage'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginBackgroundImage'] = 'EXT:ns_basetheme/Resources/Public/Images/BackendLogin/TYPO3-Rise-Background-2022.png';
+    $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginBackgroundImage'] = 'EXT:ns_basetheme/Resources/Public/Images/BackendLogin/Aadi-T3Planet-TYPO3-Login.jpg';
 }
