6aa92bddf2abd822765c3fdb49678fcd54328449
Parth Parmar <parth@nitsantech.com>
2024-07-30 18:28:13 +0530

    [Fixed] Php Warning issues By Khushi

--- Classes/ViewHelpers/ImagePreviewViewHelper.php
+++ Classes/ViewHelpers/ImagePreviewViewHelper.php
@@ -42,64 +42,33 @@ class ImagePreviewViewHelper extends AbstractViewHelper
     ) {
         $selectedValue = $arguments['selectedValue'];
         $id = $arguments['id'];
-        $baseThemeRootPath = $arguments['baseThemeRootPath'];
+        $baseThemeRootPath = $arguments['baseThemeRootPath'] ?? ''; 
         $currentThemeName = $arguments['currentThemeName'];
         $siteRootPath = $arguments['siteRootPath'];
         $value = $arguments['value'];
         $label = $arguments['label'];
         $params = $arguments['params'];
-
+    
         if (empty($currentThemeName)) {
             $objNsBasetheme = GeneralUtility::makeInstance(\NITSAN\NsBasetheme\NsBasethemeUtility::class);
             $arrAllExtensions = $objNsBasetheme->getInstalledChildTheme();
             $currentThemeName = isset($arrAllExtensions[0]) ? $arrAllExtensions[0] : '';
         }
-
-        // $idName = str_replace('.', '-', $id);
-
+    
         $instance = new self();
         list($fN, $fV, $params, $id) = $instance->ext_fNandV($arguments);
-
+    
         // Make selectBoxName dynamic
         $arrSelectBox = explode('-', $id);
-        // \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($arrSelectBox, __FILE__.' Line '.__LINE__);die;
         $selectBoxName = end($arrSelectBox);
-      
-        $NsBaseThemeRootPath = rtrim($baseThemeRootPath, '/') . '/typo3conf/ext/' . $currentThemeName . '/Resources/Public/Backend/ThemeOptionsPreview/';
-
+    
+        // Ensure $baseThemeRootPath is never null
+        $NsBaseThemeRootPath = rtrim((string)$baseThemeRootPath, '/') . '/typo3conf/ext/' . $currentThemeName . '/Resources/Public/Backend/ThemeOptionsPreview/';
+    
         $imageExtension = ($selectBoxName == 'loader') ? '.gif' : '.png';
         $previewImagePath = $NsBaseThemeRootPath . $selectBoxName . '/' . htmlspecialchars($value) . $imageExtension;
-
+    
         return $previewImagePath;
     }
-
-    public function ext_getTypeData($type)
-    {
-        $retArr = [];
-        $type = trim($type);
-        if (!$type) {
-            $retArr['type'] = 'string';
-        } else {
-            $m = strcspn($type, ' [');
-            $retArr['type'] = strtolower(substr($type, 0, $m));
-            $types = ['int' => 1, 'options' => 1, 'file' => 1, 'boolean' => 1, 'offset' => 1, 'user' => 1, 'checkbox'=>1];
-            if (isset($types[$retArr['type']])) {
-                $p = trim(substr($type, $m));
-                $reg = [];
-                preg_match('/\\[(.*)\\]/', $p, $reg);
-                if(isset($reg[1])){
-                    $p = trim($reg[1]);
-                }
-                if ($p) {
-                    $retArr['paramstr'] = $p;
-                    switch ($retArr['type']) {
-                        case 'options':
-                            $retArr['params'] = explode(',', $retArr['paramstr']);
-                            break;
-                    }
-                }
-            }
-        }
-        return $retArr;
-    }
+    
 }
