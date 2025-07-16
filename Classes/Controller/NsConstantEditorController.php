3b1c48710aed1a3ebb9ae863e701bceb6985e3a9
Mahesh Rathod <mahesh.nitsan@gmail.com>
2025-07-16 10:58:02 +0530

    [TASK] Resolved issue with TYPO3 version 13.4.15

--- Classes/Controller/NsConstantEditorController.php
+++ Classes/Controller/NsConstantEditorController.php
@@ -166,7 +166,8 @@ class NsConstantEditorController extends AbstractTemplateModuleController
         $this->eventDispatcher->dispatch(new ModifyPageLayoutContentEvent($request, $view));
         $view->setTitle($languageService->sL($currentModule->getTitle()), $pageRecord['title']);
         $view->getDocHeaderComponent()->setMetaInformation($pageRecord);
-        $this->addPreviewButtonToDocHeader($view, $pageUid, (int)$pageRecord['doktype']);
+        
+        $this->addPreviewButtonToDocHeader($view, $pageRecord);
         $this->addShortcutButtonToDocHeader($view, $currentModuleIdentifier, $pageRecord, $pageUid);
         if (!empty($relevantCategories)) {
             $this->addSaveButtonToDocHeader($view);
@@ -255,7 +256,7 @@ class NsConstantEditorController extends AbstractTemplateModuleController
         $view = $this->moduleTemplateFactory->create($request);
         $view->setTitle($languageService->sL($currentModule->getTitle()), $pageRecord['title']);
         $view->getDocHeaderComponent()->setMetaInformation($pageRecord);
-        $this->addPreviewButtonToDocHeader($view, $pageUid, (int)$pageRecord['doktype']);
+        $this->addPreviewButtonToDocHeader($view, $pageRecord);
         $view->makeDocHeaderModuleMenu(['id' => $pageUid]);
         $view->assignMultiple([
             'pageUid' => $pageUid,
